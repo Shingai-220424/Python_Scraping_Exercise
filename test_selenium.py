24f3d10bcfe91cab62f4e2fede2ee4b8104f9d4b
Shingai-220424 <OCA2204240009@edu.oca.ac.jp>
2024-05-18 17:39:44 +0900

    演習完了

--- test_selenium.py
+++ test_selenium.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_binary
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
 
 option = Options()
 option.add_argument('--headless')
@@ -12,5 +15,7 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 ll = [x for x in soup.text.split(' ') if len(x) > 0]
+
 for elem in ll:
-    print(elem)
+    ws.append([elem])
+wb.save("sample.xlsx")
